3e32e0a2144841d1db311c89245ace4ca4463e5d
Lianstu <370845739@qq.com>
2017-05-11 11:51:39 +0800

    完成socket 连接

--- common/models/chat.js
+++ common/models/chat.js
@@ -2,26 +2,7 @@
 //var app = require('http')
 //test1
 var loopback = require('loopback');
-//var app = loopback();
-//var http = require('http').Server(app);
-//var io = require('socket.io')(http);
-//test2
-//app.io = require('socket.io')(app.start());
-//io.on("connection",function(socket){
-//    console.log("*******connection*******")
-//})
-//console.log("connection_socket")
 module.exports = function(Chat) {
     console.log("connection_socket")
-    Chat.observe('after save', function (ctx, next) {
 
-        console.log("socket",socket)
-        if(ctx.isNewInstance){
-        }
-    })
-
-    //console.log("Chat_socket",socket)
-    //io.on("connection",function(socket){
-    //    console.log("connection_socket",socket.id)
-    //})
 };
